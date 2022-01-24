3ceef68106cee551e803b151387b9e0818ef70ae
patricio0312rev <review0312oficial@gmail.com>
2022-01-23 21:15:54 -0500

    Implementando test para fetchUser con jest mock

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import { getUser } from './get-user';
+import { mocked } from 'ts-jest/utils';
+
+jest.mock('./get-user');
+const mockGetUser = mocked(getUser, true);
 
 describe("When everything is OK", () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     render(<App />);
+    await waitFor(() => expect(mockGetUser).toHaveBeenCalled())
   });
 
   test("Should render the app component without crashing", () => {
@@ -42,5 +49,16 @@ describe("When everything is OK", () => {
 
   test("Should not find the role 'whatever' in our component", () => {
     expect(screen.queryByRole('whatever')).toBeNull();
+  });
+});
+
+describe("When the component fetches the user successfully", () => {
+  beforeEach(() => {
+    mockGetUser.mockClear();
+  });
+
+  test('Should call getUser once', async () => {
+    render(<App />);
+    await waitFor(() => expect(mockGetUser).toHaveBeenCalledTimes(1));
   });
 });
