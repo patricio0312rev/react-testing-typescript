9cea22be746ab8ee9a708ed602278419dc09545b
patricio0312rev <review0312oficial@gmail.com>
2022-01-23 20:09:29 -0500

    Utilizando metodo beforeach para renderizar componente

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,15 @@ import { text } from 'stream/consumers';
 import App from './App';
 
 describe("When everything is OK", () => {
-  test("Should render the app component without crashing", () => {
+  beforeEach(() => {
     render(<App />);
+  });
+
+  test("Should render the app component without crashing", () => {
     screen.debug();
   }); 
 
   test("Should select the children that is being passed to the CustomInput component", () => {
-    render(<App />);
     screen.getByText('Input:');
     screen.getByText(/Input/);
 
@@ -23,18 +25,15 @@ describe("When everything is OK", () => {
   });
 
   test("Should select the input element by its role", () => {
-    render(<App />);
     screen.getByRole('textbox');
     expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 
   test("Should select a label element by its text", () => {
-    render(<App />);
     screen.getByLabelText('Input:');
   });
 
   test("Should select input element by placeholder text", () => {
-    render(<App />);
     screen.getByPlaceholderText('Example');
-  })
+  });
 });
