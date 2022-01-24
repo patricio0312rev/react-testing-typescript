67d63112099af0fe376b5c6794eea40350915e5a
patricio0312rev <review0312oficial@gmail.com>
2022-01-23 19:52:22 -0500

    Creando test para App.js

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,5 @@ import App from './App';
 describe("When everything is OK", () => {
   test("Should render the app component without crashing", () => {
     render(<App />);
-    screen.debug();
   }); 
 });
