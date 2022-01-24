b1104463c3f25ef45967effc39a40a89745f42aa
patricio0312rev <review0312oficial@gmail.com>
2022-01-23 20:00:27 -0500

    Creando prueba para verificar text

--- src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-describe("When everything is OK", () => {
-  test("Should render the app component without crashing", () => {
-    render(<App />);
-  }); 
-});
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,4 +6,17 @@ describe("When everything is OK", () => {
     render(<App />);
     screen.debug();
   }); 
+
+  test("Should select the children that is being passed to the CustomInput component", () => {
+    render(<App />);
+    screen.getByText('Input:');
+
+    let error;
+    try {
+      screen.getByText('Input');
+    } catch(err) {
+      error = err;
+    }
+    expect(error).toBeDefined();
+  });
 });
